031544f782801ed610f2dce45b5458752cc466ba
nachomirsol <nachomirsol@gmail.com>
2019-09-13 21:44:12 +0200

    finish converting to hooks, review edit product when not typing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-class Header extends Component {
-  render() {
-    return (
-      <nav className="navbar navba-expand-lg navbar-dark bg-primary justify-content-between d-flex">
-        <h1>
-          <Link to={"/"} className="text-light">
-            CRUD - React, Redux, REST API
+const Header = () => {
+  return (
+    <nav className="navbar navba-expand-lg navbar-dark bg-primary justify-content-between d-flex">
+      <h1>
+        <Link to={"/"} className="text-light">
+          CRUD - React, Redux, REST API
           </Link>
-        </h1>
+      </h1>
 
-        <Link to={"/products/new"} className="btn btn-danger new-post">
-          Add product
+      <Link to={"/products/new"} className="btn btn-danger new-post">
+        Add product
         </Link>
-      </nav>
-    );
-  }
+    </nav>
+  );
 }
 export default Header;
--- src/containers/EditProduct/EditProduct.js
+++ src/containers/EditProduct/EditProduct.js
@@ -1,107 +1,97 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { showProduct, editProduct } from "../../actions/products";
 
-class EditProduct extends Component {
-  state = {
-    name: "",
-    price: "",
-    error: false
-  };
+const EditProduct = ({ product, showProduct, editProduct, history, match }) => {
 
-  componentDidMount() {
-    const { id } = this.props.match.params;
-    this.props.showProduct(id);
+  const [error, setError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+
+    showProduct(match.params.id);
+
+  }, [showProduct, match.params.id, product])
+
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   }
 
-  componentWillReceiveProps(nextProps, nextState) {
-    const { name, price } = nextProps.product;
-    this.setState({
-      name,
-      price
-    });
-  }
 
-  onChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  editProduct = e => {
+  const onSubmit = e => {
     e.preventDefault();
-    const { name, price } = this.state;
+    editItem();
+  };
+
+  const editItem = () => {
+
+    const { name, price } = formData;
     if (name === "" || price === "") {
-      this.setState({ error: true });
+      setError(true);
       return;
     }
 
-    this.setState({ error: false });
+    setError(false);
 
     // Get id from product
-    const { id } = this.props.match.params;
+    formData.id = match.params.id
+    console.log(formData)
 
-    const productInfo = {
-      id,
-      name,
-      price
-    };
-    this.props.editProduct(productInfo);
-    this.props.history.push("/");
+    editProduct(formData);
+    history.push("/");
   };
 
-  render() {
-    const { error, name, price } = this.state;
-    return (
-      <div className="row justify-content-center mt-5">
-        <div className="col-md-8">
-          <div className="card">
-            <div className="card-body">
-              <h2 className="text-center">Add Product</h2>
-              <form onSubmit={this.editProduct}>
-                <div className="form-group">
-                  <label>Title</label>
-                  <input
-                    onChange={this.onChange}
-                    type="text"
-                    className="form-control"
-                    placeholder="Title"
-                    name="name"
-                    defaultValue={name}
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Product price</label>
-                  <input
-                    onChange={this.onChange}
-                    type="text"
-                    className="form-control"
-                    placeholder="Price"
-                    name="price"
-                    defaultValue={price}
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
-                >
-                  Save Changes
+  return (
+
+    <div className="row justify-content-center mt-5">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center">Edit Product</h2>
+            <form onSubmit={e => onSubmit(e)}>
+              <div className="form-group">
+                <label>Title</label>
+                <input
+                  onChange={e => onChange(e)}
+                  type="text"
+                  className="form-control"
+                  placeholder="Title"
+                  name="name"
+                  defaultValue={product ? product.name : ''}
+                />
+              </div>
+              <div className="form-group">
+                <label>Product price</label>
+                <input
+                  onChange={e => onChange(e)}
+                  type="text"
+                  className="form-control"
+                  placeholder="Price"
+                  name="price"
+                  defaultValue={product ? product.price : ''}
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Save Changes
                 </button>
-              </form>
-              {error ? (
-                <div className="font-weight-bold alert alert-danger text-center mt-4">
-                  Todos los campos son obligatorios
+            </form>
+            {error ? (
+              <div className="font-weight-bold alert alert-danger text-center mt-4">
+                Todos los campos son obligatorios
                 </div>
-              ) : (
+            ) : (
                 ""
               )}
-            </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  )
 }
+
 const mapStateToProps = state => ({
   product: state.products.product
 });
